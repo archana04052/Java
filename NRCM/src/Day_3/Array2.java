e93fa400631de025e35dda9f1d136f9c6cbea0d9
DELL <DELL@DESKTOP-UQH0VHV>
2025-05-19 10:48:38 +0530

    My first commit

---
+++ NRCM/src/Day_3/Array2.java
@@ -0,0 +1,12 @@
+package Day_3;
+
+public class Array2 {
+	public static void main(String args[]) {
+		int a[]= {23,45,56,78};
+		for(int i=0;i<a.length;i++) {
+		System.out.println(a[i]);
+		
+		
+	}
+	}
+}
---
+++ NRCM/src/Day_3/Array3.java
@@ -0,0 +1,14 @@
+package Day_3;
+
+public class Array3 {
+	public static void main(String args[]) {
+		int a[]= {23,45,56,78};
+		int count=0;
+		for(int i=0;i<a.length;i++) {
+			count++;
+		System.out.println(count);
+		
+		
+	}
+	}
+}
